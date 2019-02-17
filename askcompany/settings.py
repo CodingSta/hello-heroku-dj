4148f425fe96610548b4bc2b94babfa998af61e5
Chinseok Lee <me@askcompany.kr>
2019-02-17 18:08:43 +0900

    static/media 서빙 설정

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fmc^z_@gbie8xk4*!8n5p4wkwy@ksnnkr_oj=zlpa%&bq%i&@g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
