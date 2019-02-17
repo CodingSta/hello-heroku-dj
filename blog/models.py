486d504be52bcb393cb5cb895ad2fe5710c83f11
Chinseok Lee <me@askcompany.kr>
2019-02-17 18:29:43 +0900

    blog.Comment 모델 추가

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,11 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
