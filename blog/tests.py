1a4b2b7d2c52ac2e79fbb89a4d0e36adce36fb90
Chinseok Lee <me@askcompany.kr>
2019-02-17 21:40:42 +0900

    테스트 구현

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,17 @@
+from django.shortcuts import resolve_url
 from django.test import TestCase
 
-# Create your tests here.
+
+class BlogTestCase(TestCase):
+    def test_index(self):
+        response = self.client.get(resolve_url('blog:index'))
+        self.assertEqual(response.status_code, 200)
+
+
+class CalculateTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_1(self):
+        self.assertEqual(1+1, 2)
+
